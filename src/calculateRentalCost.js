fbb240d776ca9716ac7372d930fcf7038056192c
Yegor <egor598000@gmail.com>
2025-11-23 17:55:40 +0100

    added constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,14 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  const total = days * 40;
+  const DAILY_RENTAL_RATE = 40;
+  const SMALL_RENTAL_THRESHOLD_DAYS = 3;
+  const LONG_RENTAL_THRESHOLD_DAYS = 7;
+  const SMALL_RENTAL_DISCOUNT = 20;
+  const LONG_RENTAL_DISCOUNT = 50;
 
-  if (days >= 7) {
-    return total - 50;
+  const total = days * DAILY_RENTAL_RATE;
+
+  if (days >= LONG_RENTAL_THRESHOLD_DAYS) {
+    return total - LONG_RENTAL_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return total - 20;
+  if (days >= SMALL_RENTAL_THRESHOLD_DAYS) {
+    return total - SMALL_RENTAL_DISCOUNT;
   }
 
   return total;
